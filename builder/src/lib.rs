9374b8f787c9f7df8c0debe24889383bf93829fa
inmzhang <zhangyiming21@mail.ustc.edu.cn>
2023-07-05 20:59:22 +0800

    Fix clippy warning

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -22,7 +22,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let builder_fields = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-        if ty_inner_type("Option", ty).is_some() || builder_of(&f).is_some() {
+        if ty_inner_type("Option", ty).is_some() || builder_of(f).is_some() {
             quote! { #name: #ty }
         } else {
             quote! { #name: ::std::option::Option<#ty> }
@@ -32,7 +32,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
     // impl `builder()` method
     let build_empty = fields.iter().map(|f| {
         let name = &f.ident;
-        if builder_of(&f).is_some() {
+        if builder_of(f).is_some() {
             quote! { #name: ::std::vec::Vec::new() }
         } else {
             quote! { #name: ::std::option::Option::None }
@@ -45,7 +45,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
         let ty = &f.ty;
         let (arg_ty, value) = if let Some(inner_ty) = ty_inner_type("Option", ty) {
             (inner_ty, quote! { ::std::option::Option::Some(#name) })
-        } else if builder_of(&f).is_some() {
+        } else if builder_of(f).is_some() {
             (ty, quote! { #name })
         } else {
             (ty, quote! { ::std::option::Option::Some(#name) })
@@ -57,7 +57,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
             }
         };
 
-        match extend_method(&f) {
+        match extend_method(f) {
             None => set_method,
             Some((true, extend_method)) => extend_method,
             Some((false, extend_method)) => {
@@ -132,18 +132,12 @@ fn ty_inner_type<'a>(wrapper: &str, ty: &'a syn::Type) -> Option<&'a syn::Type> 
 }
 
 fn builder_of(f: &syn::Field) -> Option<&syn::Attribute> {
-    for attr in &f.attrs {
-        if attr.path().is_ident("builder") {
-            return Some(attr);
-        }
-    }
-    None
+    f.attrs.iter().find(|&attr| attr.path().is_ident("builder"))
 }
 
 fn extend_method(f: &syn::Field) -> Option<(bool, proc_macro2::TokenStream)> {
     let name = f.ident.as_ref().unwrap();
     let g = builder_of(f)?;
-
 
     let mut extend_name: LitStr = LitStr::new("", proc_macro2::Span::call_site());
     match g.parse_nested_meta(|meta| {
@@ -153,10 +147,13 @@ fn extend_method(f: &syn::Field) -> Option<(bool, proc_macro2::TokenStream)> {
             extend_name = s;
             Ok(())
         } else {
-            Err(syn::Error::new_spanned(meta.path, "expected `builder(each = \"...\")`"))
+            Err(syn::Error::new_spanned(
+                meta.path,
+                "expected `builder(each = \"...\")`",
+            ))
         }
     }) {
-        Err(e) => return Some((false, e.to_compile_error())),
+        Err(e) => Some((false, e.to_compile_error())),
         Ok(()) => {
             let inner_ty = ty_inner_type("Vec", &f.ty).unwrap();
             let extend_ident = syn::Ident::new(&extend_name.value(), extend_name.span());
@@ -166,7 +163,7 @@ fn extend_method(f: &syn::Field) -> Option<(bool, proc_macro2::TokenStream)> {
                     self
                 }
             };
-            return Some((&extend_ident == name, method));
+            Some((&extend_ident == name, method))
         }
     }
 }
