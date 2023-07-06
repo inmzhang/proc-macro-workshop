5446774e560c26623f25fb3dba9587da3fb212e7
inmzhang <zhangyiming21@mail.ustc.edu.cn>
2023-07-06 09:35:30 +0800

    Finish stage04

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,10 +1,11 @@
 use proc_macro::TokenStream;
-use syn::{
-    parse::{Parse, ParseStream},
-    parse_macro_input, Ident, LitInt, Result, Token, braced
-};
+use proc_macro2::{Group, Literal, TokenTree};
 use quote::quote;
-use proc_macro2::{TokenTree, Group, Literal};
+use syn::{
+    braced,
+    parse::{Parse, ParseStream},
+    parse_macro_input, Ident, LitInt, Result, Token,
+};
 
 struct Seq {
     index: Ident,
@@ -42,9 +43,9 @@ pub fn seq(input: TokenStream) -> TokenStream {
     } = parse_macro_input!(input as Seq);
     let lower = lower.base10_parse::<usize>().unwrap();
     let upper = upper.base10_parse::<usize>().unwrap();
-    let repeated_contents = (lower..upper).map(|n|{
+    let repeated_contents = (lower..upper).map(|n| {
         let content = replace_n_in_token_stream(content.clone(), &index, n);
-        quote!{
+        quote! {
             #content
         }
     });
@@ -58,7 +59,8 @@ fn replace_n_in_token_stream(
     n: usize,
 ) -> proc_macro2::TokenStream {
     let mut new_token_stream = proc_macro2::TokenStream::new();
-    for tt in token_stream.into_iter() {
+    let mut peekable_iter = token_stream.into_iter().peekable();
+    while let Some(tt) = peekable_iter.next() {
         let new_tt = match tt {
             TokenTree::Group(group) => {
                 let delimiter = group.delimiter();
@@ -73,7 +75,34 @@ fn replace_n_in_token_stream(
                 lit_int.set_span(ident2.span());
                 TokenTree::Literal(lit_int)
             }
-            tt => tt,
+            TokenTree::Ident(ident2) => {
+                if let Some(next_tt) = peekable_iter.next_if(|tt| {
+                    if let TokenTree::Punct(punct) = tt {
+                        if punct.as_char() == '~' {
+                            return true;
+                        }
+                    }
+                    false
+                }) {
+                    if peekable_iter.next_if(|tt| {
+                        if let TokenTree::Ident(ident3) = tt {
+                            if *ident3 == *ident {
+                                return true;
+                            }
+                        }
+                        false
+                    }).is_some() {
+                        let new_ident = Ident::new(&format!("{}{}", ident2, n), ident2.span());
+                        TokenTree::Ident(new_ident)
+                    } else {
+                        new_token_stream.extend(std::iter::once(TokenTree::Ident(ident2)));
+                        next_tt
+                    }
+                } else {
+                    TokenTree::Ident(ident2)
+                }
+            }
+            _ => tt,
         };
         new_token_stream.extend(std::iter::once(new_tt));
     }
